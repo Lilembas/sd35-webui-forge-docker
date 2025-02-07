6c0bc7feffe6efa813d10c2915c06b919bf44bf9
Lilembas <49973615+Lilembas@users.noreply.github.com>
2025-02-07 08:33:16 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ if [ ! -d "/app/sd-webui" ] || [ ! "$(ls -A "/app/sd-webui")" ]; then
 
   if [ "$UI" = "forge-sd35" ]; then
     echo "Using Forge"
-    git clone -b sd35 https://github.com/lllyasviel/stable-diffusion-webui-forge.git /app/sd-webui
+    git clone -b sd35 --single-branch https://github.com/lllyasviel/stable-diffusion-webui-forge.git /app/sd-webui
     cd /app/sd-webui
   fi
 
